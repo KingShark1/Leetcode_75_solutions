00cce606ca98098c569412f5896f23ff14a72765
KingShark1 <manastiwari28@gmail.com>
2022-09-17 22:05:02 +0530

    practise sqrt by binary

---
+++ Coding_interview_study_plan/math/sqrt.py
@@ -0,0 +1,18 @@
+from re import X
+
+
+def mySqrt(x: int) -> int:
+	if x == 1:
+		return 1
+
+	result = x
+
+	while(result*result > x):
+		result = (result + x // result) >> 1
+	
+	return result
+	
+	
+
+n = int(input("Enter Number : "))
+print(f"Square root of number : {mySqrt(n)}")
